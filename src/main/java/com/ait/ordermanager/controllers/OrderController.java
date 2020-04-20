ac99533deefd66873a794f8c5db992c76a65cc0f
dmica <dmitrovic.maja@gmail.com>
2020-04-20 11:36:59 +0100

    exception_handeling_and_status_codes

--- src/main/java/com/ait/ordermanager/controllers/OrderController.java
+++ src/main/java/com/ait/ordermanager/controllers/OrderController.java
@@ -1,12 +1,16 @@
 package com.ait.ordermanager.controllers;
-
 import com.ait.ordermanager.dao.OrderDAO;
 import com.ait.ordermanager.dto.Order;
+import com.ait.ordermanager.exceptions.OrderNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class OrderController
@@ -18,5 +22,48 @@ public class OrderController
     public List<Order> getAllOrders()
     {
         return orderDAO.findAll();
+    }
+
+    @GetMapping("orders/{orderId}")
+    public Order getWPhoneById(@PathVariable int orderId)
+    {
+        Optional<Order> foundOrder = orderDAO.findById(orderId);
+        if(foundOrder.isPresent())
+            return foundOrder.get();
+        else
+            throw new OrderNotFoundException("Unable to retrieve order with id:" + orderId);
+    }
+
+    @DeleteMapping("orders/{orderId}")
+    public void deleteOrderById(@PathVariable int orderId)
+    {
+        orderDAO.deleteOrder(orderId);
+    }
+
+    @PostMapping("orders/")
+    public ResponseEntity createOrder(@RequestBody Order newOrder)
+    {
+        orderDAO.createOrder(newOrder);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("{id}")
+                .buildAndExpand(newOrder.getId()).toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    @PostMapping("orders/")
+    public ResponseEntity updateOrder(@RequestBody Order newOrder)
+    {
+        boolean updated = orderDAO.updateOrder(newOrder);
+        if(updated)
+        {
+            return ResponseEntity.status(HttpStatus.OK).build();
+        }
+        else
+        {
+            URI location = ServletUriComponentsBuilder
+                    .fromCurrentRequest().path("{id}")
+                    .buildAndExpand(newOrder.getId()).toUri();
+            return ResponseEntity.created(location).build();
+        }
     }
 }
--- src/main/java/com/ait/ordermanager/dao/OrderDAO.java
+++ src/main/java/com/ait/ordermanager/dao/OrderDAO.java
@@ -1,6 +1,7 @@
 package com.ait.ordermanager.dao;
 
 import com.ait.ordermanager.dto.Order;
+import com.ait.ordermanager.exceptions.OrderNotFoundException;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -24,7 +25,7 @@ public class OrderDAO {
             new Order(10, "Yvonne Smith", "765 Sandymount Drive", "Dublin", "Republic of Ireland", 2)
     ));
 
-    private Order findPhone(int orderId)
+    private Order findOrder(int orderId)
     {
         Order found = null;
         for (Order order : orderList)
@@ -45,7 +46,43 @@ public class OrderDAO {
 
     public Optional<Order> findById(int id)
     {
-        return Optional.ofNullable(findPhone(id));
+        Order foundOrder = findOrder(id);
+        if(foundOrder != null)
+            return Optional.ofNullable(foundOrder);
+        else
+            throw new OrderNotFoundException("Unable to find order with id: " +id);
+    }
+
+    public void deleteOrder(int id)
+    {
+        Order foundOrder = findOrder(id);
+        if(foundOrder != null)
+            orderList.remove(foundOrder);
+        else
+            throw new OrderNotFoundException("Unable to find order with id: " + id);
+    }
+
+    public void createOrder(Order newOrder)
+    {
+        orderList.add(newOrder);
+    }
+
+    public boolean updateOrder(Order order)
+    {
+        boolean updated = true;
+        Order foundOrder = findOrder(order.getId());
+        if(foundOrder != null)
+        {
+            int index = orderList.indexOf(foundOrder);
+            orderList.remove(index);
+            orderList.add(index, order);
+        }
+        else
+        {
+            orderList.add(order);
+            updated = false;
+        }
+        return updated;
     }
 }
 
